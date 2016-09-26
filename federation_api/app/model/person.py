97667561a6d42f3df7cdac7692e5072dcb4d85a0
alienware <tanay.upadhyaya@practo.com>
2016-09-26 20:53:49 +0000

    Fab polish changes

--- federation_api/app/model/person.py
+++ federation_api/app/model/person.py
@@ -1,12 +1,13 @@
 from federation_api.app.model import StarFleet
 from app import db
 
+
 class Person(StarFleet, db.Model):
     id = db.Column(db.Integer, nullable=False, primary_key=True)
     email = db.Column(db.String(length=128), nullable=True, unique=True,
-                                index=True)
+                      index=True)
     phone = db.Column(db.String(length=128), nullable=True, unique=True,
-                                index=True)
+                      index=True)
     account_id = db.Column(db.String(length=128), nullable=True, unique=True,
-                                     index=True)
+                           index=True)
     name = db.Column(db.String(length=128), nullable=True)
