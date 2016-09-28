66eac1b8e990c64530a84d57cb47d07a1bcfb837
alienware <tanay.upadhyaya@practo.com>
2016-09-28 18:16:56 +0000

    Routes are loaded back in the request context; fixes routes.py to print routes in app context

--- routes.py
+++ routes.py
@@ -1,9 +1,12 @@
 import urllib
 from flask import url_for
 from app import app
+from config.routes import blueprints
 
 output = []
 app.config['SERVER_NAME'] = '<HOST_NAME>:<HOST_PORT>'
+for blueprint_name, blueprint_url_prefix in blueprints:
+    app.register_blueprint(blueprint_name, url_prefix=blueprint_url_prefix)
 
 with app.app_context():
     for rule in app.url_map.iter_rules():
