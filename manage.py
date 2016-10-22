b43e555a0b15c825e5bdab9268b499ab2f006c98
alienware <tanay.upadhyaya@practo.com>
2016-10-22 22:17:20 +0000

    Moves routes to manage script

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
-from flask_script import Manager, Server, Shell
+import urllib
+from flask import url_for
+from flask_script import Manager, Server, Shell, Command
 from config.app import create_app
 from config.db import db
 from config.initializers.newrelic_monitoring import NewrelicMonitoring
@@ -18,6 +20,28 @@ def _make_context():
     return dict(app=create_app(), db=db, **models)
 manager.add_command('shell', Shell(make_context=_make_context))
 
+
+class Routes(Command):
+    def run(self):
+        output = []
+        app = manager.app
+
+        for rule in app.url_map.iter_rules():
+            options = {}
+            for arg in rule.arguments:
+                options[arg] = "[{0}]".format(arg)
+
+            methods = ','.join(rule.methods)
+            # FIXME: Results in http://<host_name>:<host_port>/<blueprint_mount>/<endpoint>g
+            url = url_for(rule.endpoint, **options)
+            line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods,
+                                                            url))
+            output.append(line)
+
+        for line in sorted(output):
+            print(line)
+manager.add_command('routes', Routes())
+
+
 if __name__ == '__main__':
     manager.run()
-
